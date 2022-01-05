3cec5d8e5d53d5dfa9a85cf84e9d9ea7cfcb639f
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-01-05 20:56:27 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
-    return (
-        <Layout>
-            <h1>About page</h1>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Doloribus dolorum quam est omnis totam soluta sint velit
+              dignissimos ratione quas
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing. eligendi quia
+              facere
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person pouring salt in a Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
+    </Layout>
+  )
 }
 
 export default About
